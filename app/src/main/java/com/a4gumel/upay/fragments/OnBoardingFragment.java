c5f81c1f77b5a1f6566c72cbd0acecad3ecaf883
auwal <a4auwal@gmail.com>
2022-09-25 13:24:19 +0100

    onboarding_layout ui

--- app/src/main/java/com/a4gumel/upay/fragments/OnBoardingFragment.java
+++ app/src/main/java/com/a4gumel/upay/fragments/OnBoardingFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 import com.a4gumel.upay.R;
 
-public class IntroFragment extends Fragment {
+public class OnBoardingFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,6 @@ public class IntroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false);
+        return inflater.inflate(R.layout.fragment_onboarding, container, false);
     }
 }
